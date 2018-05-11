295ae86d6d13a4a2b0778985d47ddbdb5a6f3169
Irfan Ali <jd1985@post.com>
2018-05-11 12:08:33 -0400

    added mqtt

--- cloud/smart-home-provider-cloud.js
+++ cloud/smart-home-provider-cloud.js
@@ -391,6 +391,49 @@ app.get('/getauthcode', function (req, resp) {
       '');
   }
 });
+
+/* @@@@@@@@@@@@@@@@@@@@@@@@ MQTT Code Here @@@@@@@@@@@@@@@@@@@@@@@ */
+
+const MqttClient = {};
+
+var mqtt = require('mqtt'), url = require('url');
+// Parse
+var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
+var auth = (mqtt_url.auth || ':').split(':');
+// Create a client connection
+MqttClient.client = mqtt.connect(mqtt_url, {
+    username: auth[0],
+    password: auth[1]
+});
+
+MqttClient.client.on('connect', function() { // When connected
+
+    // subscribe to a topic
+    MqttClient.client.subscribe('hello/world', function() {
+        // when a message arrives, do something with it
+        MqttClient.client.on('message', function(topic, message, packet) {
+            console.log("Received '" + message + "' on '" + topic + "'");
+        });
+    });
+
+    // publish a message to a topic
+    MqttClient.client.publish('/feeds/irSend', 'my random message', function() {
+        console.log("Message is published");
+        MqttClient.client.end(); // Close the connection when published
+    });
+});
+
+app.post('/smart-home-api/sendMessage', function (request, response) {
+    // console.log('post /smart-home-api/status');
+    MqttClient.client.publish('/feeds/irSend', 'message from button', function() {
+        console.log("Message from button");
+        MqttClient.client.end(); // Close the connection when published
+    });
+
+
+});
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
 app.use('/frontend', express.static('./frontend'));
 app.use('/frontend/', express.static('./frontend'));
 app.use('/', express.static('./frontend'));
--- index.js
+++ index.js
@@ -3,33 +3,36 @@
  */
 
 require('./cloud/smart-home-provider-cloud.js');
+/*
+
+const MqttClient = {};
 
 var mqtt = require('mqtt'), url = require('url');
 // Parse
 var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
 var auth = (mqtt_url.auth || ':').split(':');
 // Create a client connection
-const options = {
-
-};
-var client = mqtt.connect(mqtt_url, {
+MqttClient.client = mqtt.connect(mqtt_url, {
     username: auth[0],
     password: auth[1]
 });
 
-client.on('connect', function() { // When connected
+
+
+MqttClient.client.on('connect', function() { // When connected
 
     // subscribe to a topic
-    client.subscribe('hello/world', function() {
+    MqttClient.client.subscribe('hello/world', function() {
         // when a message arrives, do something with it
-        client.on('message', function(topic, message, packet) {
+        MqttClient.client.on('message', function(topic, message, packet) {
             console.log("Received '" + message + "' on '" + topic + "'");
         });
     });
 
     // publish a message to a topic
-    client.publish('/feeds/irSend', 'my random message', function() {
+    MqttClient.client.publish('/feeds/irSend', 'my random message', function() {
         console.log("Message is published");
-        client.end(); // Close the connection when published
+        MqttClient.client.end(); // Close the connection when published
     });
 });
+exports.MqttClient = MqttClient;*/
