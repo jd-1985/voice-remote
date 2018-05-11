71fb666ba8643453e88f5a4523b16f6cc2307978
Irfan Ali <jd1985@post.com>
2018-05-11 12:13:13 -0400

    Added mqtt

--- cloud/smart-home-provider-cloud.js
+++ cloud/smart-home-provider-cloud.js
@@ -401,36 +401,29 @@ var mqtt = require('mqtt'), url = require('url');
 var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
 var auth = (mqtt_url.auth || ':').split(':');
 // Create a client connection
-MqttClient.client = mqtt.connect(mqtt_url, {
-    username: auth[0],
-    password: auth[1]
-});
-
-MqttClient.client.on('connect', function() { // When connected
-
-    // subscribe to a topic
-    MqttClient.client.subscribe('hello/world', function() {
-        // when a message arrives, do something with it
-        MqttClient.client.on('message', function(topic, message, packet) {
-            console.log("Received '" + message + "' on '" + topic + "'");
-        });
-    });
-
-    // publish a message to a topic
-    MqttClient.client.publish('/feeds/irSend', 'my random message', function() {
-        console.log("Message is published");
-        MqttClient.client.end(); // Close the connection when published
-    });
-});
-
 app.post('/smart-home-api/sendMessage', function (request, response) {
     // console.log('post /smart-home-api/status');
-    MqttClient.client.publish('/feeds/irSend', 'message from button', function() {
-        console.log("Message from button");
-        MqttClient.client.end(); // Close the connection when published
+    MqttClient.client = mqtt.connect(mqtt_url, {
+        username: auth[0],
+        password: auth[1]
     });
 
+    MqttClient.client.on('connect', function() { // When connected
 
+        // subscribe to a topic
+        MqttClient.client.subscribe('hello/world', function() {
+            // when a message arrives, do something with it
+            MqttClient.client.on('message', function(topic, message, packet) {
+                console.log("Received '" + message + "' on '" + topic + "'");
+            });
+        });
+
+        // publish a message to a topic
+        MqttClient.client.publish('/feeds/irSend', 'my random message', function() {
+            console.log("Message is published");
+            MqttClient.client.end(); // Close the connection when published
+        });
+    });
 });
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 
