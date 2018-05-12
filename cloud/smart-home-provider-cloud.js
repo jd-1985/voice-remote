3cb2705dfa727afd9377e9e33558c1e298f6ff48
Irfan Ali <jd1985@post.com>
2018-05-11 21:34:27 -0400

    Fixed code issue

--- cloud/smart-home-provider-cloud.js
+++ cloud/smart-home-provider-cloud.js
@@ -400,13 +400,16 @@ var mqtt = require('mqtt'), url = require('url');
 // Parse
 var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
 var auth = (mqtt_url.auth || ':').split(':');
+MqttClient.client = mqtt.connect(mqtt_url, {
+    username: auth[0],
+    password: auth[1]
+});
 // Create a client connection
 app.post('/smart-home-api/sendMessage', function (request, response) {
     // console.log('post /smart-home-api/status');
-    MqttClient.client = mqtt.connect(mqtt_url, {
-        username: auth[0],
-        password: auth[1]
-    });
+    if(!MqttClient.client.connected){
+      MqttClient.client.reconnect();
+    }
     console.log("****************" + JSON.stringify(request.body));
 
     MqttClient.client.on('connect', function() { // When connected
